7586978d3fb066924c94c5eb32d195f049cc1f0b
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2019-11-08 23:00:31 +0100

    Refactorize classes in mars.py

--- mars.py
+++ mars.py
@@ -15,48 +15,28 @@ class MarsDataLoader:
     def __init__(self,nasa_key):
         
         self.nasa_key = nasa_key
-    
-    def load_mars_data_from_api(self):
-        # methods to download data from Nasa Insight
-        data = requests.get(f'https://api.nasa.gov/insight_weather/?api_key={nasa_key}&feedtype=json&ver=1.0')
-        content = data.text
-        content = json.loads(content)
-        return content
+        data_from_nasa_api = requests.get(f'https://api.nasa.gov/insight_weather/?api_key={nasa_key}&feedtype=json&ver=1.0')
+        content = data_from_nasa_api.json()
+        self.content = content 
 
 class DayAtMars:
 
-    def __init__(self, data_from_mars):
-        self.data_from_mars = data_from_mars
+    def __init__(self, content):
+        self.today = content['sol_keys'][0]
+        self.temperature_on_mars = content[self.today]['AT']['av']
+        self.speed_of_wind_on_mars = content[self.today]['HWS']['av']
+        self.pressure_on_mars = content[self.today]['PRE']['av']
 
-    
-    def get_today(self,data_from_mars):
-        today = data_from_mars['sol_keys'][0]
-        return today
-    
-   
-    def get_temp_on_mars(self,data_from_mars):
-        mars_temp = data_from_mars[self.get_today(data_from_mars)]['AT']['av']
-        return mars_temp
-    
-
-    def get_speed_of_wind_on_mars(self, data_from_mars):
-        mars_wind_speed = data_from_mars[self.get_today(data_from_mars)]['HWS']['av']
-        return mars_wind_speed
-    
-
-    def get_pressure_on_mars(self, data_from_mars):
-        press_on_mars = data_from_mars[self.get_today(data_from_mars)]['PRE']['av']
-        return press_on_mars
-
-    def set_weather_info(self,mars_temp,mars_wind_speed,press_on_mars):
+    def create_weather_on_mars_information(self):
         massage =f'''Goood morning! Today is going to be sunny day on Elysium Platinia. There will be no clouds. 
-        Temperature outside: {mars_temp}, light wind with {mars_wind_speed} m/s. Air pressure is {press_on_mars} Pa. 
+        Temperature outside: {self.temperature_on_mars}, light wind with {self.speed_of_wind_on_mars} m/s. Air pressure is {self.pressure_on_mars} Pa. 
         Unfortunately there is still no chance to survive outside on Mars. So brace yourself 
         and prepare for another beautifull day on Earth. In case you ARE on Mars... so sunny 
         weather but still you are in a deep shit if you are outside without a spacesuit on.'''
         return massage
 
 class CreateMessage(MarsDataLoader,DayAtMars):
+    # Consider to delete that class
     
     def __init__(self):
         pass
@@ -64,14 +44,11 @@ class CreateMessage(MarsDataLoader,DayAtMars):
     
     def create_message(self):
         loader = MarsDataLoader(nasa_key)
-        mars_data = loader.load_mars_data_from_api()
-        day_at_mars = DayAtMars(mars_data)
-        #today = day_at_mars.get_today
-        temp_at_mars = day_at_mars.get_temp_on_mars(mars_data)
-        pressure_at_mars = day_at_mars.get_pressure_on_mars(mars_data)
-        wind_speed_at_mars = day_at_mars.get_speed_of_wind_on_mars(mars_data)
-        message_with_weathercast_from_mars = day_at_mars.set_weather_info(temp_at_mars,wind_speed_at_mars,pressure_at_mars)
-        return message_with_weathercast_from_mars
+        day_at_mars = DayAtMars(loader.content)
+        message = day_at_mars.create_weather_on_mars_information()
+
+        return message
+
 
 
 
--- readme.md
+++ readme.md
@@ -3,5 +3,6 @@
 Program connecting to NASA API for daily information from Insigt Mars Lander about weather on Mars. It gets temperature, wind speed, and pressure. Insight Mars Lander is located on Elysium Platinia. It's a place on Mars very close to the equator of Mars. Additionally application is going to send daily information at time choosen by user to some comunnicator
 
 ## Install Application
+
 1. Clone repository
 2. Install requirements.txt pip install -r requirements.txt
